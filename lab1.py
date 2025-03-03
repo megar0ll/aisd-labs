1ce93a73db714978f880259007a5c606dce1215d
ромчик абрамов <roll.btw.roll@gmail.com>
2025-03-03 13:56:58 +0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,14 +1,14 @@
-digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'} #словарь
 
-def num2words(num):
+def num2words(num): #функция превращения цифры в ее прописную вариацию
     return ' '.join(digit_words[int(d)] for d in str(num)) if num != 0 else 'ноль'
 
-def transform_even_number(word):
+def transform_even_number(word): #функция трансформации числа путем смены двух соседних цифр
     return ''.join([word[i+1] + word[i] for i in range(0, len(word)-1, 2)]) + (word[-1] if len(word) % 2 else '')
-k = int(input('Введите количество нулей:'))
+k = int(input('Введите количество нулей:')) #кол-во 0-ей в числе, встретив которое процесс трансформации прервется
 fl = 0
 A,B=[],[]
-with open('input.txt', 'r') as f:
+with open('input.txt', 'r') as f: 
     words = f.read().split()
     for word in words:
         if word.isdigit():
