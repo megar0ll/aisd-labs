c271f9d71a66a1780fbcd40316cbc629118c6667
ромчик абрамов <roll.btw.roll@gmail.com>
2025-03-27 18:28:28 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,28 +1,12 @@
 import re
-K = 3
-F = 0
-digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
-A, B=[], []
-def num2words(num):
-    return ' '.join(digit_words[int(d)] for d in str(num)) if num != 0 else 'ноль'
-def transform_even_number(word):
-    return ''.join([word[i + 1] + word[i] for i in range(0, len(word) - 1, 2)]) + (word[-1] if len(word) % 2 else '')
-def process_numbers(text, F):
-    number_pattern = re.compile(r'\b\d+\b')
-    zero_pattern = re.compile(r'0' * K)
-    for match in number_pattern.finditer(text):
-        word = match.group()
-        if word.isdigit():
-            if int(word) % 2 != 0 and len(word) > 3:
-                A.append(int(word))
-            if int(word) % 2 == 0:
-                transformed = transform_even_number(word)
-                if F>0 or zero_pattern.search(transformed):
-                    F += 1
-                    B.append(num2words(int(word)))
-                if F==0:
-                    B.append(transformed)
-    print(f"Первый список: {A}\nВторой список: {B}")
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-    process_numbers(text, F)
+d = {0:'ноль',1:'один',2:'два',3:'три',4:'четыре',5:'пять',6:'шесть',7:'семь',8:'восемь',9:'девять'}
+k = 3
+A, B, f = [], [], False
+for w in re.findall(r'\d+', open('input.txt').read()): #чтение файла
+    n = int(w)
+    if n%2 and len(w)>3: A.append(n)
+    if n%2==0:
+        t = re.sub(r'(\d)(\d)', r'\2\1', w[:-1]) + (w[-1] if len(w)%2 else '') #смена соседних символов
+        if f or '0'*k in t: B.append(' '.join(d[int(c)] for c in w)); f = True
+        else: B.append(t)
+print(f"1:{A}\n2:{B}")
