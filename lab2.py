cca0c16c8e5c47c5e131479dafa9cc30c2aae3c5
ромчик абрамов <roll.btw.roll@gmail.com>
2025-04-28 14:33:24 +0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,13 +1,10 @@
 import re
-d = {0:'ноль',1:'один',2:'два',3:'три',4:'четыре',5:'пять',6:'шесть',7:'семь',8:'восемь',9:'девять'}
-k = 3
-text = open('input.txt').read()
-A = [int(w) for w in re.findall(r'\d{4,}', text) if int(w)%2]
-nums = re.findall(r'\d+', text)
-B, f = [], False
-for w in nums:
-    if int(w)%2 == 0:
-        t = re.sub(r'(\d)(\d)', r'\2\1', w[:-1]) + w[-1]*(len(w)%2)
-        if f or re.search(f'0{{{k}}}', t): B.append(' '.join(d[int(c)] for c in w)); f = True
-        else: B.append(t)
-print(f"1:{A}\n2:{B}")
+digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+def num2words(num): return ' '.join(digit_words[int(d)] for d in str(num))
+with open('input.txt', 'r') as f:
+    numbers = re.findall(r'\b\d{0,5}\b', f.read())
+    minzn, maxzn = min(map(int, numbers)), max(map(int, numbers))
+    for num_str in numbers:
+        if num_str.startswith('77') and len(num_str) > 2:
+            print(num_str[2:])
+print('Среднее число =', num2words((minzn + maxzn) // 2))
