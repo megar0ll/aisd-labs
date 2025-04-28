be6cd7becc84cf2642adea9381de8aaf7c5ccb4b
ромчик абрамов <roll.btw.roll@gmail.com>
2025-04-28 14:40:26 +0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,10 +1,17 @@
 import re
-digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
-def num2words(num): return ' '.join(digit_words[int(d)] for d in str(num))
+
+digit_words = {10: 'ЮЛЬ', 1: 'ОДИН', 2: 'два', 3: 'три', 4: 'четыре', 
+               5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+
+def num2words(num):
+    return ''.join(digit_words[int(d)] for d in str(num))
+
 with open('input.txt', 'r') as f:
-    numbers = re.findall(r'\b\d{0,5}\b', f.read())
-    minzn, maxzn = min(map(int, numbers)), max(map(int, numbers))
-    for num_str in numbers:
-        if num_str.startswith('77') and len(num_str) > 2:
-            print(num_str[2:])
+    numbers = re.findall(r'\b77\d{1,5}\b', f.read())  # 77 + до 5 цифр (всего до 7 символов)
+    
+for num_str in numbers:
+    if num_str.startswith('77') and len(num_str) > 2:
+        print(num_str[2:])
+        
+minzn, maxzn = min(map(int, numbers)), max(map(int, numbers))
 print('Среднее число =', num2words((minzn + maxzn) // 2))
